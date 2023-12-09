b5393bff8b73b9eb6dfd78b2960606060a74b2cb
villageworks <villageworks@gmail.com>
2023-12-10 08:38:46 +0900

    ver.1.32: 管理者ログイン処理

--- src/main/java/com/example/demo/controller/admin/AccountAdminController.java
+++ src/main/java/com/example/demo/controller/admin/AccountAdminController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AccountAdminController {
@@ -10,6 +13,22 @@ public class AccountAdminController {
 	public String index() {
 		// 画面遷移
 		return "admin/login";
+	}
+	
+	// 管理者ログイン処理
+	@PostMapping("/admin/login")
+	public String login(
+			@RequestParam("name") String name,
+			@RequestParam("password") String password,
+			Model model) {
+		// 管理者用ログイン処理
+		boolean notAuthenticated = !("admin".equals(name) && "himitu".equals(password));
+		if (notAuthenticated) {
+			model.addAttribute("error", "ユーザ名とパスワードが一致しませんでした");
+			return "/admin/login";
+		}
+		// 画面遷移
+		return "redirect:/admin/items";
 	}
 	
 }
