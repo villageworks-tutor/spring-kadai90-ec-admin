707cc6521d5bd1d1b93c6fbd61f6d18ba0af8574
villageworks <villageworks@gmail.com>
2023-12-11 22:07:39 +0900

    ver.1.42: 注文情報表示

--- src/main/java/com/example/demo/controller/admin/CustomerAdminController.java
+++ src/main/java/com/example/demo/controller/admin/CustomerAdminController.java
@@ -6,15 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.entity.Customer;
+import com.example.demo.entity.Order;
+import com.example.demo.entity.OrderDetail;
 import com.example.demo.repository.CustomerRepository;
+import com.example.demo.repository.OrderDetailRepository;
+import com.example.demo.repository.OrderRepository;
 
 @Controller
 public class CustomerAdminController {
 	
 	@Autowired
 	CustomerRepository customerRepository;
+	@Autowired
+	OrderRepository orderRepository;
+	@Autowired
+	OrderDetailRepository orderDetailRepository;
 	
 	// 顧客一覧画面表示
 	@GetMapping("/admin/customers")
@@ -26,4 +35,30 @@ public class CustomerAdminController {
 		// 画面遷移
 		return "admin/customers";
 	}
-}
+	
+	// 注文情報画面表示
+	@GetMapping("/admin/customers/{id}/orders")
+	public String show(
+			@PathVariable("id") Integer id,
+			Model model) {
+		// パスパラメータをもとに顧客インスタンスを取得
+		Customer customer = customerRepository.findById(id).get();
+		// 取得した顧客インスタンスをスコープに登録
+		model.addAttribute("customer", customer);
+		// 顧客の注文履歴を取得
+		List<Order> orderList= orderRepository.findByCustomerId(id);
+		// 取得した注文リストをスコープに登録
+		model.addAttribute("orderList", orderList);
+		// 注文履歴の先頭要素の詳細を取得
+		if (orderList != null && orderList.size() > 0) {
+			// 注文履歴の戦闘要素を取得
+			Order order = orderList.get(0);
+			// IDに紐づいた明細を取得
+			List<OrderDetail> detailList = orderDetailRepository.findByOrderId(order.getId());
+			// 取得した注文リストをスコープに登録
+			model.addAttribute("detailList", detailList);
+		}
+		// 画面遷移
+		return "admin/orders.html";
+	}
+ }
